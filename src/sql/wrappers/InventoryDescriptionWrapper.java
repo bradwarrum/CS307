7a560991c99d550f5a5e27c2476874513ea1b1df
Evan Rogers <rogerse@purdue.edu>
2015-03-05 19:42:27 -0500

    Fixed Reference Issue
    Should now compile

--- src/sql/wrappers/InventoryDescriptionWrapper.java
+++ src/sql/wrappers/InventoryDescriptionWrapper.java
@@ -3,6 +3,7 @@ package sql.wrappers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.google.gson.*;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
@@ -14,9 +15,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
+import java.net.*;
 import java.nio.charset.Charset;
-
-import json.JSONObject;
 
 import sql.SQLExecutable;
 import sql.SQLParam;
@@ -45,22 +45,35 @@ public class InventoryDescriptionWrapper extends SQLExecutable{
 			results= query("SELECT Description FROM InventoryItem WHERE (UPC=? AND HouseholdId=?);",upcsql,hidp);
 			if(results==null){
 				//hit API for default description
-				//first one has 1000 hits per day
-				
+							
 				String surl= "http://api.upcdatabase.org/json/72aaf1c920ed0cd53c54c6bc52b4c7ad/"+UPC;
-				JsonObject rootobj = readJsonFromUrl(surl); 
+				URL url = new URL(surl);
+				HttpURLConnection request = (HttpURLConnection) url.openConnection();
+				request.connect();
+
+				JsonParser jp = new JsonParser(); //from gson
+			    JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //convert the input stream to a json element
+			    JsonObject rootobj = root.getAsJsonObject(); 
 			    String res=rootobj.get("valid").getAsString();
 								
 				if(!res.equals("false")){
-					description=rootobj.get("itemname");
+					description=rootobj.get("itemname").getAsString();
 				}else{
 					description= "";
 				}
-				//TODO: add more api hits in case there is over 1000 a day
+				//TODO: add more api hits in case there is over 1000 a day or buy more hits
 			}else{
 				description=results.getString(1);			
 			}
 		}catch (SQLException e){
+			release(results);
+			release();
+			return false;
+		}catch (MalformedURLException e){
+			release(results);
+			release();
+			return false;
+		}catch (Exception e){
 			release(results);
 			release();
 			return false;
